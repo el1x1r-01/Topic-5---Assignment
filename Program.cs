f8c253a17dceb236047cb64fe01923e11083502a
drago <drago@EM-LOPTAP>
2025-09-17 09:20:45 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,113 @@
+﻿namespace Topic_5___Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string programNumber;
+
+            Console.WriteLine("Which program would you like to run? (Part 1 / Part 2 / Part 3)");
+            programNumber = Console.ReadLine();
+
+            Console.WriteLine("");
+
+            if (programNumber.ToLower() == "part 1" || programNumber == "1")
+            {
+                Part1();
+            }
+
+            else if (programNumber.ToLower() == "part 2" || programNumber == "2")
+            {
+                Part2();
+            }
+
+            else if (programNumber.ToLower() == "part 3" || programNumber == "3")
+            {
+                Part3();
+            }
+
+            Console.ReadLine();
+        }
+
+        public static void Part1()
+        {
+            int planet;
+            double earthWeight, planetWeight;
+
+            Console.WriteLine("Welcome, Julio Cesar Chavez Mark VII.");
+            Console.WriteLine("");
+
+            Console.Write("Please enter your current Earth weight: ");
+            double.TryParse(Console.ReadLine(), out earthWeight);
+
+            Console.WriteLine("");
+            Console.WriteLine("I have information for the following planets:");
+            Console.WriteLine("");
+            Console.WriteLine("\t 1. Venus \t 2. Mars \t 3. Jupiter");
+            Console.WriteLine("\t 4. Saturn \t 5. Uranus \t 6. Neptune");
+
+            Console.WriteLine("");
+            Console.WriteLine("Which planet are you visiting?");
+            Int32.TryParse(Console.ReadLine(), out planet);
+
+            Console.WriteLine("");
+
+            if (planet == 1)
+            {
+                planetWeight = Math.Round((earthWeight * 0.78), 1);
+
+                Console.WriteLine("Your weight on Venus would be " + planetWeight + " lbs.");
+            }
+
+            else if (planet == 2)
+            {
+                planetWeight = Math.Round((earthWeight * 0.39), 1);
+
+                Console.WriteLine("Your weight on Mars would be " + planetWeight + " lbs.");
+            }
+
+            else if (planet == 3)
+            {
+                planetWeight = Math.Round((earthWeight * 2.65), 1);
+
+                Console.WriteLine("Your weight on Jupiter would be " + planetWeight + " lbs.");
+            }
+
+            else if (planet == 4)
+            {
+                planetWeight = Math.Round((earthWeight * 1.17), 1);
+
+                Console.WriteLine("Your weight on Saturn would be " + planetWeight + " lbs.");
+            }
+
+            else if (planet == 5)
+            {
+                planetWeight = Math.Round((earthWeight * 1.05), 1);
+
+                Console.WriteLine("Your weight on Uranus would be " + planetWeight + " lbs.");
+            }
+
+            else if (planet == 6)
+            {
+                planetWeight = Math.Round((earthWeight * 1.23), 1);
+
+                Console.WriteLine("Your weight on Neptune would be " + planetWeight + " lbs.");
+            }
+
+            else
+            {
+                Console.WriteLine("That is not a valid planet.");
+            }
+        }
+
+        public static void Part2()
+        {
+            Console.WriteLine();
+        }
+
+        public static void Part3()
+        {
+
+        }
+    }
+}
